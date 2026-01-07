57a16d3fd4845606d9f10a41cf973aace2cde2c7
76Claudia <claudinha.gcruz@gmail.com>
2026-01-06 23:02:29 -0300

    Tests findAll

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,20 +1,69 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 
 public class MovieControllerRA {
 	
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken, invalidToken;
+	private Long existingMovieId, nonExistingMovieId;
+	private String movieTitle;
+	
+	
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";
+		
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "alex@gmail.com";
+		adminPassword = "123456";
+		
+		existingMovieId = 13L;
+		movieTitle = "Vingadores: Ultimato";
+		
+		
+	}
+	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		
+		given()
+			.get("/movies")
+			
+		.then()
+			.statusCode(200);			
 	}
 	
 	@Test
 	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+		
+		
+		given()
+		  	.get("/movies?title={movieTitle}", movieTitle)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(13))
+			.body("content.title[0]", equalTo("Vingadores: Ultimato"))
+			.body("content.score[0]", is(0.0F))	
+			.body("content.count[0]", is(0))
+			.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg"));
+			
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {	
+		
+		
+		
 	}
 	
 	@Test
